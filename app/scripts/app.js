a9dcf276997fe82398201fa41e78fd17bb203055
andylee589 <andylee589@gmail.com>
2014-08-13 18:13:09 +0800

    init with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stringDetectorWebClientAngularApp
+ * @description
+ * # stringDetectorWebClientAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stringDetectorWebClientAngularApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stringDetectorWebClientAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stringDetectorWebClientAngularApp
+ */
+angular.module('stringDetectorWebClientAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
